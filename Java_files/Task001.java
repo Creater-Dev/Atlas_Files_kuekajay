08b5d1acd00eedece35c3fd8dad8f43b83b9b0e3
Kadiyala Ajay Kumar <90555293+Creater-Dev@users.noreply.github.com>
2025-06-08 14:30:46 +0530

    Delete Java_files directory

--- Java_files/Task001.java
+++
@@ -1,9 +0,0 @@
-package Java_files;
-
-public class Task001 { // This is a simple Java program that prints "Hello, World!" to the console.
-    // The main method is the entry point of the program.
-    public static void main(String[] args) { //main method
-        // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
-    }
-}
